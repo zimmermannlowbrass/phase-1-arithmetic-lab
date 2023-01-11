df502f3db6b92a9fd9ef0d4a0e074f3571522f9f
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-11 12:31:12 -0600

    completed labwork

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,5,20);
